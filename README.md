5d3235ad96c1e7d8a36ec1994a45cfaa78e6bbda
sangjunPark <juntech0421@github.com>
2020-05-01 21:25:52 +0900

    modify add.js

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ css 작업 및 app.js 코드 추가
 app.js : onmouse/mousedown/mouseup/leavemouse 을
 이용하여 캔버스 내에서의 마우스의 움직임 값을
 받는 코드 작성. 콘솔 로그를 통해 클릭시 paint가 시작되는 것과 클릭을 유지하지 않거나 캔버스 밖으로 벗어나는 경우 paint 작업이 종료되는 것을 확인함.
+
+app.js add code: canvas의 getContext("2d");를 통해
+painting이 false일 때 Path를 가져와 offset 좌표값을 읽고 있다가 painting이 true가 되면 path가 마지막에 있던 좌표부터 저장된 strokeStyle의 정보로 선을 긋는다. painting이 true가 되는 구간은 mousedown일 때 이다. 오후 09시 14분
--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const canvas = document.getElementById("js-canvas");
+const ctx = canvas.getContext("2d");
 
+canvas.width = 700; //실제 canvas의 pixel size
+canvas.height = 450;//실제 canvas의 pixel size
+
+ctx.strokeStyle = "rgb(0, 0, 0)";
+ctx.lineWidth = 2.5;
 let painting = false;
+
+function paintStart() {
+    painting = true;
+    console.log("paint");
+}
 
 function paintStop() {
     painting = false;
@@ -9,21 +20,24 @@ function paintStop() {
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event) {
-    painting = true;
-    console.log("paint");
+    startPaint();
 }
 
-
-function onMouseUp(event) {
-   paintStop();
-}
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", paintStart);
+    canvas.addEventListener("mouseup", paintStop);
     canvas.addEventListener("mouseleave", paintStop);
 }
